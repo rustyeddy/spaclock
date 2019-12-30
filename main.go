b155ff1e081ff76e0de69cd051bf5cfb550990fa
Rusty Eddy <rusty@rustyeddy.com>
2019-12-30 15:47:18 -0800

    We have established a websocket echo

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // spaHandler implements the http.Handler interface, so we can use it
@@ -19,6 +21,15 @@ type spaHandler struct {
 	staticPath string
 	indexPath  string
 }
+
+// upgrader is used by the HTTP socket to establish a websocket
+// connection with the client
+var (
+	upgrader *websocket.Upgrader = &websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
 
 // ServeHTTP inspects the URL path to locate a file within the static dir
 // on the SPA handler. If a file is found, it will be served. If not, the
@@ -54,13 +65,40 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
+func handleUpgrade(w http.ResponseWriter, r *http.Request) {
+
+	up := &websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+	conn, err := up.Upgrade(w, r, nil)
+	if err != nil {
+		log.Errorf("Websocket Upgrade failed %v", err)
+		return
+	}
+	defer conn.Close()
+
+	// ... Use conn to send a recieve messages: this basically turns
+	// into an echo server.
+	for {
+		mtype, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Errorf("Websocket Read failed %v", err)
+			return
+		}
+		log.Debugf("ws recieved client %v %p", mtype, msg)
+
+		if err := conn.WriteMessage(mtype, msg); err != nil {
+			log.Errorf("Websocket Write failed %v", err)
+			return
+		}
+	}
+}
+
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	})
+	router.HandleFunc("/api/health", handleHealth)
+	router.HandleFunc("/ws", handleUpgrade)
 
 	spa := spaHandler{staticPath: "pub", indexPath: "index.html"}
 	router.PathPrefix("/").Handler(spa)
@@ -74,4 +112,9 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	// an example API handler
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
--- pub/clock.js
+++ pub/clock.js
@@ -1,4 +1,5 @@
 function startTime() {
+    /*
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -8,7 +9,33 @@ function startTime() {
     document.getElementById('clock').innerHTML =
 	"<span id='clock' class='text-center'>" + h + ":" + m + ":" + s + "</span>";
     var t = setTimeout(startTime, 500);
+    */
+    var clock = document.getElementById("clock");
+
+    if (ws) {
+	return
+    }
+    //var ws = new WebSocket("ws://{{ .Host }}/ws");
+    var ws = new WebSocket("ws://localhost:8000/ws")
+    ws.onopen = function(evt) {
+	console.log("websocket opening")
+    }
+
+    ws.onclose = function(evt) {
+	data.textContent = 'Connection Closed';
+    }
+
+    ws.onmessage = function(evt) {
+	console.log('update over websocket');
+	data.textContent = evt.Data
+    }
+
+    ws.onerror = function(evt) {
+        print("ERROR: " + evt.data);
+    }
+
 }
+
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
